f35c6ff9c3940c5cd4a6f31552ee1ba9437be4e8
melbin <njanmelbin@gmail.com>
2016-06-12 08:16:28 +0530

    Working gulp which reloads for js and html changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,6 @@
 var gulp = require('gulp'),
 	browserSync= require('browser-sync').create();
 
-gulp.task('serve',function(){
-	browserSync.init({
-        server: "./src"
-    });
-//watch index file
-    // gulp.watch('src/*.html').on('change', browserSync.reload);
-});
 
 gulp.task('copy',function(){
 		gulp.src('src/*.html')
@@ -16,15 +9,26 @@ gulp.task('copy',function(){
 
 // js-watch reloads the browser with js changes
 
-gulp.task('js-watch',browserSync.reload);
-gulp.task('html-watch',['copy'],browserSync.reload);
+gulp.task('js-watch',function(){
+	browserSync.reload();
+});
+gulp.task('html-watch',['copy'],function(){
+	browserSync.reload();
+});
 
-gulp.task('watch',function(){
-// if html files changes copy task is called
+// gulp.task('watch',function(){
+// });
+gulp.task('serve',function(){
+	browserSync.init({
+        server: "./src"
+    });
+//watch index file
+    // gulp.watch('src/*.html').on('change', browserSync.reload);
+    // if html files changes copy task is called
 	gulp.watch('src/*.html',['html-watch']);
 //watch js files and calls js-watch
 	gulp.watch('src/js/*.js',['js-watch']);
+
 });
 
-
-gulp.task('default',['watch','serve','copy']);
+gulp.task('default',['serve']);
