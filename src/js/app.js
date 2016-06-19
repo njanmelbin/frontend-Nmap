61327efbe37a723e07f9bb83596cf47327d6d122
melbin <njanmelbin@gmail.com>
2016-06-19 15:34:54 +0530

    creates a observable array with result data

--- src/js/app.js
+++ src/js/app.js
@@ -47,48 +47,67 @@ var initialPlaces = {
 }
 var Place = function(data){
    // console.log(data);
-   this.lat = ko.observable(data.geometry.location.lat());
-   this.lng = ko.observable(data.geometry.location.lng());
+   this.lat = ko.observable(data.location.lat);
+   this.lng = ko.observable(data.location.lng);
+   this.id = ko.observable(data.categories.id);
    this.name = ko.observable(data.name);
+   var mycor = new google.maps.LatLng(this.lat(),this.lng());
+    // console.log(mycor);
+   var marker = new google.maps.Marker({
+        position: mycor,
+        map : map
+    });
+   this.marker = ko.observable(marker);
+   // console.log(this.marker());
+    // console.log("marker created");
+    // console.log(marker);
+    
+
+   // console.log(data);
+   // console.log(this.lat());
 };
 
 var ViewModel = function(){
 	// console.log("hai ");
 	var self =this;
 	this.myquery = ko.observable();
-	this.mylist = ko.observableArray([]);
+	// this.mylist = ko.observableArray([]);
     this.placeList = ko.observableArray([]);
 
-    initialPlaces.places.forEach(function(placeitem){
-    	var place=new Place(placeitem);
-    	self.placeList.push(place);
-    	// self.addObjects(place);  
-    	// console.log("inside initial push");
-    	createMarker(place);
-    	// self.addItems(place.name);
-    	self.mylist.push(place.name);
-    });
+    // initialPlaces.places.forEach(function(placeitem){
+    // 	var place=new Place(placeitem);
+    // 	self.placeList.push(place);
+    // 	// self.addObjects(place);  
+    // 	// console.log("inside initial push");
+    // 	createMarker(place);
+    // 	// self.addItems(place.name);
+    // 	self.mylist.push(place.name);
+    // });
 
-    // add the objects u recieve from google nearbysearch to an observable array
+    // add the objects u recieve from search to an observable array
     this.addObjects = function(data){
+    	// console.log("inside addobj");
     	var place = new Place(data);
     	self.placeList.push(place);
-    	createMarker(place);
+    	// createMarker(place);
     }
    // creates the list with names
-	this.addItems = function(data){
-		self.mylist.push(data);
+	// this.addItems = function(data){
+		// self.mylist.push(data);
+// 
+	// }
 
-	}
-
+    this.initialise = function(){
+    	performSearch();
+    }
 // when u type in new filter to search for and click filter button ,this function calls performSearch to search for keyword u typed
 	this.filter = function(){
-		self.mylist.removeAll();
+		// self.mylist.removeAll();
 		// console.log("filer results");
 		// console.log(this.myquery());
 		if(this.myquery()){
 			// console.log("inside filter");
-			performSearch();
+			// performSearch();
 		}
 	}
 
